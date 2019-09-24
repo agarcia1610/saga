d06e0c280a6238e6da5f52c66b86a8c3a0825975
agarcia1610 <55726487+agarcia1610@users.noreply.github.com>
2019-09-23 22:35:42 -0500

    first commit

---
+++ quiz/src/main/java/com/saganet/quiz/resource/QuizResource.java
@@ -0,0 +1,16 @@
+package com.saganet.quiz.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class QuizResource {
+	
+	@GetMapping("/hola")
+	public String hola(){
+		return "petición de Get";
+	}
+
+}
